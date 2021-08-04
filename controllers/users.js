e834bffeed8640cf4d92d25562ab78281d406b10
Valeriy Stolyar <valeriistolyar@yandex.ru>
2021-08-04 23:42:28 +0300

    bug: forgot put Fail into update

--- controllers/users.js
+++ controllers/users.js
@@ -75,6 +75,7 @@ module.exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id, { name, about }, { new: true, runValidators: true },
   )
+    .orFail(new Error('NotValidId'))
     .then((user) => {
       res.send({ data: user });
     })
@@ -112,6 +113,7 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id, { avatar }, { new: true, runValidators: true },
   )
+    .orFail(new Error('NotValidId'))
     .then((user) => {
       res.send({ data: user });
     })
