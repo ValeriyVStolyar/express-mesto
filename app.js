e5fc1d3b15b4dac8b2f1d82133b3758eb674091e
Valeriy Stolyar <valeriistolyar@yandex.ru>
2021-08-21 17:52:43 +0300

    feat: Cors methods

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.use(function(req, res, next) {
   console.log(origin)
 
 const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
@@ -47,7 +49,7 @@ const { method } = req; // Сохраняем тип запроса (HTTP-мет
 
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    // res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
